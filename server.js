40cf354cc79b85e99e6a0a66156e48c0309033d3
peterj330 <peter.a.jarrard@gmail.com>
2018-10-19 19:36:57 -0500

    npm installs, server set up, initial directory and file set up

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+
+// initializes Express
+var app = express();
+
+// uses morgan to log requests
+app.use(logger("dev"));
+
+// parses request body as a JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// makes the public folder static
+app.use(express.static("public"));
+
+// connects to Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// ROUTES
+// *** need to set up routes ***
+
+
+// starts the server listening
+app.listen(PORT, function() {
+    console.log(`The App is running on port ${PORT}!!!`);
+});
